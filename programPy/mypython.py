47da9bdba1571fdff6074059798eeeef06073ba3
a <a@10-197-36-181.sds.oregonstate.edu>
2019-07-26 21:06:54 -0700

    changes to python program

--- programPy/mypython.py
+++ programPy/mypython.py
@@ -2,11 +2,13 @@ import os       #for the os.system function
 import string   #to get string functions
 import random   #to get random functions
 
+# Empty String Variable
 filename1 = ""
 filename2 = ""
 filename3 = ""
 
- #source https://pynative.com/python-generate-random-string/
+# Generate Random String of length 10
+#source https://pynative.com/python-generate-random-string/
 def randomString(stringLength=10):
     """Generate a random string of fixed length """
     letters = string.ascii_lowercase
@@ -28,16 +30,16 @@ os.system(cmd3)
 cmd1 = cmd1 + " > " + filename1
 cmd2 = cmd2 + " > " + filename2
 cmd3 = cmd3 + " > " + filename3
-# os.system(cmd1)
-# os.system(cmd2)
-# os.system(cmd3)
+os.system(cmd1)
+os.system(cmd2)
+os.system(cmd3)
 
 # Generate Random Numbers
 num1 = random.randint(1,42)
 num2 = random.randint(1,42)
 
 # Print out random num to console
-cmd4 = "echo " + str(num1)
+cmd4 = "echo " + str(num1)  #cast int to string
 os.system(cmd4)
 cmd4 = "echo " + str(num2)
 os.system(cmd4)
