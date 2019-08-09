9789afc361475e6e84756bacfe9a9946b59315dd
Tuan Tran <trant6@os1.engr.oregonstate.edu>
2019-08-08 22:36:57 -0700

    changes as of 10:36pm 8/8/19; > redirection works but not <

--- program3/smallsh.c
+++ program3/smallsh.c
@@ -413,9 +413,6 @@ void statusCmd(int status)
 							inputFile = strdup(str1);
 							outputFile = strdup(str3);
 							fflush( stdout );
-							// sourceFD = open(stdin, O_RDONLY);
-							// if (sourceFD == -1) {perror("source open()"); exit(1);}
-							// printf("sourceFD == %d\n", sourceFD); // Written to terminal
 							targetFD = open(outputFile, O_WRONLY | O_CREAT | O_TRUNC, 0644); 
 							if (targetFD == -1) { perror("target open()"); exit(1); } 
 							// printf("targetFD == %d\n", targetFD); // Written to terminal
@@ -437,18 +434,29 @@ void statusCmd(int status)
 							int sourceFD, targetFD, result;
 							char * inputFile = NULL;
 							char * outputFile = NULL;
-							inputFile = strdup(str3);
-							outputFile = strdup(str1);
+							inputFile = strdup(str1);
+							outputFile = strdup(str3);
+
+							// printf("inputFile: %s\noutputFile: %s\n", inputFile, outputFile);
 							fflush( stdout );
-							sourceFD = open(inputFile, O_RDONLY);
-							if (sourceFD == -1) {perror("source open()"); exit(1);}
-							printf("sourceFD == %d\n", sourceFD); // Written to terminal
+							// sourceFD = open(inputFile, O_RDONLY);
+							// if (sourceFD == -1) {perror("source open()"); exit(1);}
+							// printf("sourceFD == %d\n", sourceFD); // Written to terminal
+
+							targetFD = open(outputFile, O_WRONLY | O_CREAT | O_TRUNC, 0644); 
+							if (targetFD == -1) { perror("target open()"); exit(1); } 
+							printf("targetFD == %d\n", targetFD); // Written to terminal
+
+							fflush( stdout );
 
 							result = dup2(sourceFD, 0); 
 							if (result == -1) { perror("source dup2()"); exit(2); } 
 							result = dup2(targetFD, 1); 
 							if (result == -1) { perror("target dup2()"); exit(2); }
-							execlp(str1, str1, token1, str3, NULL);
+
+							char * args[3] = {str1, str2, str3};
+							execvp(args[0], args);
+							// execlp(str1, str1, token1, str3, NULL);
 
 						}
 						else
